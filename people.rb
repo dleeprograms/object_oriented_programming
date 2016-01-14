482600c134af9ccf6b2dbb2d8282573fc0913bd6
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-14 13:53:12 -0500

    exercise_1_4

--- people.rb
+++ people.rb
@@ -1,10 +1,14 @@
-class person
+class Person
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
+end
 
+def greeting
+  "Hi, my name is #{name}"
+end
 
 class Student
   def learn
