a02cf5cdd8d6f0eecb624287e9136d8bad72e3a2
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-14 13:47:50 -0500

    exercise_1_4

--- people.rb
+++ people.rb
@@ -1,8 +1,17 @@
+class person
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+
+
 class Student
   def learn
     "I get it"
-
+end
 
 class Instructor
   def teach
     "Everything in Ruby is an Object"
+end
