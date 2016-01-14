b1344ebd32f3874b52b4ef42de0951bd9052bbf0
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-14 15:16:00 -0500

    exercise_1

--- people.rb
+++ people.rb
@@ -1,21 +1,40 @@
 class Person
-  attr_accessor :first_name, :last_name
+  attr_accessor :name
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(name) #age)
+    @name = name
+    @greetings = greetings
+  end
+
+  def greetings
+      "Hi, my name is #{name}"
+  end
+
 end
 
-def greeting
-  "Hi, my name is #{name}"
-end
-
-class Student
+class Student < Person  # chris = Student.new # chris.learn
   def learn
     "I get it"
+  end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     "Everything in Ruby is an Object"
+  end
+
 end
+
+
+
+chris = Instructor.new("Chris")
+
+christina = Student.new("Christina")
+
+# chris = Student.new(name)
+# chris = Student.new("chris")
+
+# Because teach method does not indicate Student.
+# NoMethodError: undefined method `teach' for #<Student:0x007f84ea888dc8>
+# 	from (irb):44
+# 	from /Users/dlee/.rbenv/versions/2.3.0/bin/irb:11:in `<main>'
